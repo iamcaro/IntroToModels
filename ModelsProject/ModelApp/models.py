5cd88404fe8d7106df143eab06bcee009eecd770
Carolina <carochang@outlook.com>
2021-05-31 14:35:52 -0700

    Add Screenshots

--- ModelsProject/ModelApp/models.py
+++ ModelsProject/ModelApp/models.py
@@ -6,28 +6,46 @@ from django.db import models
 class School (models.Model):
     name = models.CharField(max_length=400)
 
+    def __str__(self):
+        return self.name
+
 
 class CertificateType (models.Model):
     name = models.CharField(max_length=400)
 
+    def __str__(self):
+        return self.name
+
+
 class Grade (models.Model):
     type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.type
 
 
 class Faculty (models.Model):
     name = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
 
 
 class Department (models.Model):
     name = models.CharField(max_length=400)
     faculty = models.ForeignKey(Faculty, on_delete=models.CommaSeparatedIntegerField)
 
+    def __str__(self):
+        return self.name
+        
 
 class Student (models.Model):
     full_name = models.CharField(max_length= 100)
     grad_year = models.IntegerField()
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT)
+    grade = models.ForeignKey(Grade, on_delete=models.PROTECT)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
-    certificate_type = models.ForeignKey(CertificateType, on_delete=models.CASCADE)
-    
+    certificate_type = models.ForeignKey(CertificateType, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.full_name
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # IntroToModels
+
+## Admin Panel
+![Admin Panel](Screenshots/AdminPanel.png)
+
+## Certificate Types
+![Certificate Types](Screenshots/CertTypes.png)
+
+## Departments
+![Departments](Screenshots/Departments.png)
+![Department Details](Screenshots/DepartmentDetails.png)
+
+## Faculties
+![Faculties](Screenshots/Faculties.png)
+
+## Grades
+![Grades](Screenshots/Grades.png)
+
+## School
+![School](Screenshots/School.png)
+
+## Students
+![Students](Screenshots/Students.png)
+![Student Details](Screenshots/StudentDetails.png)
+
